2b7f3878a9420d131e4b70432fe5b3c61d1f7a81
MilleniumMan <jksangeethchandran@gmail.com>
2018-06-15 21:06:24 +0530

    fb sdk

---
+++ Assets/FBManager.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Facebook.Unity;
+
+public class FBManager : MonoBehaviour
+{
+    public Text txtStatus;
+   List<string> perms = new List<string>() { "public_profile", "email" };
+    void Awake()
+    {
+        if (!FB.IsInitialized)
+        {
+            // Initialize the Facebook SDK
+            FB.Init(InitCallback, OnHideUnity);
+        }
+        else
+        {
+            // Already initialized, signal an app activation App Event
+            FB.ActivateApp();
+        }
+    }
+
+    private void InitCallback()
+    {
+        if (FB.IsInitialized)
+        {
+            // Signal an app activation App Event
+            FB.ActivateApp();
+            // Continue with Facebook SDK
+            // ...
+        }
+        else
+        {
+            Debug.Log("Failed to Initialize the Facebook SDK");
+            txtStatus.text = "Failed to Initialize the Facebook SDK";
+        }
+    }
+
+    private void OnHideUnity(bool isGameShown)
+    {
+        if (!isGameShown)
+        {
+            // Pause the game - we will need to hide
+            Time.timeScale = 0;
+        }
+        else
+        {
+            // Resume the game - we're getting focus again
+            Time.timeScale = 1;
+        }
+    }
+    private void AuthCallback(ILoginResult result)
+    {
+        if (FB.IsLoggedIn)
+        {
+            // AccessToken class will have session details
+            var aToken = Facebook.Unity.AccessToken.CurrentAccessToken;
+            // Print current access token's User ID
+            Debug.Log(aToken.UserId);
+            txtStatus.text = "Your user id is"+aToken.UserId;
+            // Print current access token's granted permissions
+            foreach (string perm in aToken.Permissions)
+            {
+                Debug.Log(perm);
+            }
+        }
+        else
+        {
+            Debug.Log("User cancelled login");
+            txtStatus.text = "User cancelled login";
+        }
+    }
+    public void FBLogin()
+    {
+        FB.LogInWithReadPermissions(perms, AuthCallback);
+    }
+}
